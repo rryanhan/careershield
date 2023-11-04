f2e0976a13c2613fa461c996c814accb5c6fce96
ryanhan <ryanjhan10@gmail.com>
2023-11-04 16:55:17 -0700

    commit

--- src/components/legal/Legal.jsx
+++ src/components/legal/Legal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar'
+import Aibot from "./Aibot"
 import "./legal.css"
 
 const Legal = () => {
@@ -10,10 +11,12 @@ const Legal = () => {
     <div className='legal-main'>
       <div className= "legal-header"> Legal Documentation</div>
       <div className= "legal-title">AI Employment-Orientated Chatbot</div>
-      <div className= "legal-description"> Use our chatbot to get resources and links directly about the issues you are facing in your workplace, whether it is regarding harassment, lack of employee benefits, overworking, and more!</div>
-
-      
+      <div className= "legal-description"> Use our chatbot to get resources and links directly about the issues you are facing in your workplace, whether it is regarding harassment, lack of employee benefits, overworking, and more!</div>  
     </div>
+    <div className='legal-chatbot'>
+      <Aibot/>
+    </div>
+  
     </div>
 
   )
